518f775cd9c17d41536442b09821c2064b0f2b7f
David <d3brown3@gmail.com>
2018-12-17 19:01:29 -0500

    adding for loop for scraping

--- src/YFscraper.py
+++ src/YFscraper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 
 import re
@@ -9,16 +9,18 @@ import requests
 from io import StringIO
 from datetime import datetime, timedelta
 import pandas as pd
+import urllib
+import gzip
 
 
-# In[2]:
+# In[6]:
 
 
 # credit:
 # https://stackoverflow.com/questions/44225771/scraping-historical-data-from-yahoo-finance-with-python?rq=1
 
 
-# In[3]:
+# In[7]:
 
 
 class YFH: #YFH = YahooFinanceHistory
@@ -54,13 +56,87 @@ class YFH: #YFH = YahooFinanceHistory
         
 
 
-# In[4]:
+# In[8]:
 
 
-df = YFH('AAPL', days_back=365*5).get_quote()
-df['Symbol'] = 'AAPL'
+# df = YFH('AAPL', days_back=30).get_quote()
+# df['Symbol'] = 'AAPL'
+# cols = df.columns.tolist()
+# cols = cols[-1:] + cols[:-1]
+# df = df[cols]
+# df
+
+
+# In[9]:
+
+
+import csv
+nasdaq_url = 'https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download'
+nyse_url = 'https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download'
+amex_url = 'https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download'
+
+nasdaq_csv = urllib.request.urlopen(nasdaq_url)
+nyse_csv = urllib.request.urlopen(nyse_url)
+amex_csv = urllib.request.urlopen(amex_url)
+
+nasdaq = pd.read_csv(nasdaq_csv)
+nyse = pd.read_csv(nyse_csv)
+amex = pd.read_csv(amex_csv)
+
+nasdaq['Exchange'] = 'nasdaq'
+nyse['Exchange'] = 'nyse'
+amex['Exchange'] = 'amex'
+
+symbols = pd.DataFrame(columns=nyse.columns.tolist())
+
+
+# In[10]:
+
+
+symbols = nasdaq
+# cols = symbols.columns.tolist()
+# cols = cols[-1:] + cols[:-1]
+# symbols = symbols[cols]
+
+symbols.append(nyse)
+symbols.append(amex)
+
+exchanges = list(symbols['Exchange'])
+symbols = list(symbols['Symbol'])
+
+# print(symbols[0:9], exchanges[0:9])
+
+
+# In[33]:
+
+
+df = pd.DataFrame()
+
+for i in symbols:
+    print(i)
+    
+    try:
+        df_i = YFH(i, days_back=365*10).get_quote()
+        df_i['Symbol'] = i
+        df = df.append(df_i)
+        print(i)
+    except: ## skipping when there is a cookie / crumb issue with yahoo finance...
+        pass
+
 cols = df.columns.tolist()
 cols = cols[-1:] + cols[:-1]
 df = df[cols]
-df
+
+
+# In[34]:
+
+
+df.to_csv("/home/david/stockPortfolioGen/data/stocks.csv.gz", compression='gzip')
+
+
+# In[35]:
+
+
+df = pd.read_csv('/home/david/stockPortfolioGen/data/stocks.csv.gz', compression='gzip')
+# df
 
